ddda4a187ae295c1c59636c40acd5a10f18045ef
ak4700159 <ak47001@naver.com>
2024-07-07 14:41:56 +0900

    hello

--- hello.c
+++ hello.c
@@ -2,5 +2,10 @@
 
 int main(void){
 	printf("Hello World\n");
+	pritnf("second commit\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	pritnf("second commit\n");
+	pritnf("second commit\n");
 	return 0;
 }
